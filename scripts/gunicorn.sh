9628a77eaa24ada868a7dc223b4f6d5a0a713600
Shankar9542 <shankar.chinthapally66@gmail.com>
2025-03-20 16:46:22 +0530

    Fix line endings for gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -7,5 +7,5 @@ cd /home/ubuntu/SPORTSAPPLICATION
 # Activate virtual environment
 source /home/ubuntu/env/bin/activate  
 
-# Run Gunicorn with your app
-exec /home/ubuntu/env/bin/gunicorn --workers 3 --bind 0.0.0.0:8000 wsgi:app  # Adjust as needed
+# Run Gunicorn with Django's WSGI module
+exec /home/ubuntu/env/bin/gunicorn --workers 3 --bind 0.0.0.0:8000 SportMeet.wsgi:application  # Update with your project name

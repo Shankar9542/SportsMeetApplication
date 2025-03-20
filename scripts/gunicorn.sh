dd612c31d3a67da5a8632a458d837f975e2df472
Shankar9542 <shankar.chinthapally66@gmail.com>
2025-03-20 18:49:15 +0530

    Fixed line endings and updated files

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting Gunicorn..."
 
-cd /home/ubuntu/SPORTSAPPLICATION || exit  
-source /home/ubuntu/env/bin/activate  
+cd /home/ubuntu/SPORTSAPPLICATION
+source /home/ubuntu/env/bin/activate
 
-exec gunicorn --chdir /home/ubuntu/SPORTSAPPLICATION --workers 3 --bind 0.0.0.0:8000 SportMeet.wsgi:application --log-file - 2>&1
+exec gunicorn --workers 3 --bind 0.0.0.0:8000 SportMeet.wsgi:application --log-file /home/ubuntu/gunicorn.log 2>&1
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -7,11 +7,11 @@ python manage.py makemigrations
 python manage.py collectstatic
 sudo service gunicorn restart
 sudo service nginx restart
-#sudo tail -f /var/log/nginx/error.log
+sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
 #sudo nginx -t
-#sudo systemctl restart gunicorn
+sudo systemctl restart gunicorn
 #sudo systemctl status gunicorn
 #sudo systemctl status nginx
 # Check the status

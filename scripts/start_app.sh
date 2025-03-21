27bbd27c0c18064b4b2093d987f9941e5d9a4d2a
Shankar9542 <shankar.chinthapally66@gmail.com>
2025-03-21 15:22:00 +0530

    Resolve changes

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
+sed -i 's/\[]/\["98.80.11.169"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     

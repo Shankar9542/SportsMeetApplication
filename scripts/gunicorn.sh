f085b03fd6b6f8e40b29d5d4101824eb67fcdb85
Shankar9542 <shankar.chinthapally66@gmail.com>
2025-03-22 16:02:47 +0530

    Changes files

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
-sudo cp /home/ubuntu/SPORTSAPPLICATION/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/SPORTSAPPLICATION/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
-sudo systemctl start gunicorn.service
-sudo systemctl enable gunicorn.service
+# Copy Gunicorn service files
+sudo cp /home/ubuntu/SPORTSAPPLICATION/gunicorn/gunicorn.socket /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/SPORTSAPPLICATION/gunicorn/gunicorn.service /etc/systemd/system/gunicorn.service
+
+# Reload systemd manager
 sudo systemctl daemon-reload
-sudo systemctl restart gunicorn
-sudo systemctl status gunicorn
 
+# Enable and start Gunicorn service
+sudo systemctl enable --now gunicorn.service
+
+# Restart Gunicorn to apply changes
+sudo systemctl restart gunicorn
+
+
+
+# Check status
+sudo systemctl status gunicorn --no-pager

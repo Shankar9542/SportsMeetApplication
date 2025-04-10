ae5905a183ba8d1075c75f2c0c31f07360cc12a7
Shankar9542 <148524491+Shankar9542@users.noreply.github.com>
2025-04-10 13:37:47 +0530

    Update instance_os_dependencies.sh

--- scripts/instance_os_dependencies.sh
+++ scripts/instance_os_dependencies.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-sudo apt install -y python3-pip
-sudo apt install -y nginx
-sudo apt install -y virtualenv
+# Update package lists
+sudo yum update -y
+
+# Install Python3 and pip
+sudo yum install -y python3 python3-pip
+
+# Install Nginx
+sudo amazon-linux-extras enable nginx1
+sudo yum install -y nginx
+
+# Install virtualenv using pip
+sudo pip3 install virtualenv

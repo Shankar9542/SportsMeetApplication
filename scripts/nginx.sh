f349a9c63f33a83ff1c4974f56708117eef345a6
Shankar9542 <148524491+Shankar9542@users.noreply.github.com>
2025-04-15 13:36:54 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,11 +6,11 @@ sudo systemctl daemon-reexec
 # Remove default Nginx config (if any)
 sudo rm -f /etc/nginx/conf.d/default.conf
 
-# Copy your custom Nginx configuration
-sudo cp /home/ec2-user/SPORTSAPPLICATION/nginx/nginx.conf /etc/nginx/conf.d/sportmeet.conf
+# Copy your custom Nginx configuration into the proper directory
+sudo cp /home/ec2-user/SPORTSAPPLICATION/nginx/nginx.conf /etc/nginx/nginx.conf
 
 # Optional: Change permissions or ownership if needed
-# sudo chown root:root /etc/nginx/conf.d/sportmeet.conf
+# sudo chown root:root /etc/nginx/nginx.conf
 
 # Test Nginx configuration
 sudo nginx -t
